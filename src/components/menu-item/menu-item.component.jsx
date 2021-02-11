eaf6b364f82602b21b5be7e2cf77b098f601bc8b
Budi Santoso <bdisanttoso@gmail.com>
2021-02-11 20:39:32 +0700

    menambahkan router menggunakan react-router-dom

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import { withRouter } from "react-router-dom";
 
 import "./menu-item.styles.scss";
 
-const MenuItem = ({ title, imageUrl, size }) => (
-  <div className={`${size} menu-item`}>
+const MenuItem = ({ title, imageUrl, size, linkUrl, history, match }) => (
+  <div
+    className={`${size} menu-item`}
+    onClick={() => history.push(`${match.url}${linkUrl}`)}
+  >
+    {console.log("ini match:")}
+    {console.log(history)}
     <div
       className="background-image"
       style={{
@@ -11,10 +17,10 @@ const MenuItem = ({ title, imageUrl, size }) => (
       }}
     ></div>
     <div className="content">
-      <h1 className="title">{title}</h1>
+      <h1 className="title">{title.toUpperCase()}</h1>
       <span className="subtitle">SHOP NOW</span>
     </div>
   </div>
 );
 
-export default MenuItem;
+export default withRouter(MenuItem);
